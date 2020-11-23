efbbd2bdccff52ae5f4ca5442650d9f75888e65d
Benoît Puech <benoit.puech@yahoo.fr>
2020-11-23 19:47:09 +0100

    Ajout d'un WHERE dans la requête sql pour n'afficher que les billets qui ont été validés

---
+++ admin.php
@@ -0,0 +1,16 @@
+<?php
+include('connexionBdd.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Accueil Admin</title>
+</head>
+<body>
+
+	<h1><center>Bienvenue sur l'espace d'Administration du blog</center></h1>
+
+</body>
+</html>
