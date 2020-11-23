004b40d3f7760cd01eb7691221dbf8fc4dcd6c30
Benoît Puech <benoit.puech@yahoo.fr>
2020-11-23 03:32:01 +0100

    Résolution de l'erreur id_billet et ajout des messages d'erreur + résolution de l'erreur de redirection

--- commentaires_post.php
+++ commentaires_post.php
@@ -2,15 +2,20 @@
 
 include('connexionBdd.php');
 
-if (isset($_POST['submit'])) 
+if (isset($_POST['submit']) AND isset($_GET['id'])) 
 {
 	if (!empty($_POST['pseudo']) AND !empty($_POST['commentaire'])) 
 	{
-		$req = $bdd->prepare('INSERT INTO commentaires(auteur, commentaire, date_commentaire) VALUES (?, ?, NOW())');
-		$req->execute(array($_POST['pseudo'],
+		$req = $bdd->prepare('INSERT INTO commentaires(id_billet, auteur, commentaire, date_commentaire) VALUES (?, ?, ?, NOW())');
+		$req->execute(array($_GET['id'],
+							$_POST['pseudo'],
 							$_POST['commentaire']));
 
-		header('Location: commentaires.php');
+		header('Location: commentaires.php?id=' . $_GET['id']);
 	}
+	else
+		echo 'Les champs sont vides.';
 }
+else
+	echo 'Erreur lors du stockage du commentaire';
 ?>
