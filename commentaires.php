79cc85e27485340d2c174674853327b513b6aaa2
Benoît Puech <benoit.puech@yahoo.fr>
2020-11-23 01:54:01 +0100

    Ajouté les htmlspecialchars là où il y en avait besoin

---
+++ commentaires.php
@@ -0,0 +1,59 @@
+<?php
+include('connexionBdd.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Mon super Blog - commentaires</title>
+</head>
+
+<body>
+
+	<h1>Mon super blog !</h1>
+
+	<p>
+		<a href="index.php">Retour à la liste des billets</a>
+	</p>
+
+	<?php
+
+	$reqBillet = $bdd->prepare("SELECT titre, contenu, DATE_FORMAT(date_creation, 'le %d/%m/%Y à %Hh%imin%ss') AS date FROM billets WHERE id= ?");
+	$reqBillet->execute(array($_GET['id']));
+
+	$donnees = $reqBillet->fetch();
+	?>
+	<h3><?php echo htmlspecialchars($donnees['titre']); ?> <em> <?php echo htmlspecialchars($donnees['date']); ?></em></h3>
+	<span class="news">
+		<p>
+			<?php echo htmlspecialchars($donnees['contenu']); ?>
+		</p>
+	</span>
+
+	<h2>Commentaires</h2>
+
+	<?php
+	if (isset($_GET['id']) AND !empty($_GET['id'])) 
+	{
+		$reqCommentaires = $bdd->prepare("SELECT auteur, commentaire, DATE_FORMAT(date_commentaire, 'le %d/%m/%Y à %Hh%imin%ss') AS date FROM commentaires WHERE id_billet= ? ORDER BY date_commentaire");
+		$reqCommentaires->execute(array($_GET['id']));
+
+		while ($donnees = $reqCommentaires->fetch()) 
+		{
+	?>
+
+	<strong><?php echo $donnees['auteur']; ?></strong> <?php echo $donnees['date']; ?><br />
+	<p>
+		<?php echo $donnees['commentaire']; ?>
+	</p>
+
+	<?php
+		}
+	}
+	?>
+
+</body>
+
+</html>
