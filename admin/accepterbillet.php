05056e32550733afc1db0e796f6c2009abce3786
Benoît Puech <benoit.puech@yahoo.fr>
2020-11-24 23:02:17 +0100

    Permet de choisir si l'admin accepte de publier ce billet ou préfère le supprimer

---
+++ admin/accepterbillet.php
@@ -0,0 +1,44 @@
+<?php
+/*
+accepterbillet.php
+------------------
+
+Affiche le billet dans son intégralité et permet de choisir si on veut le publier ou le supprimer.
+
+Last update : 24/11/2020
+*/
+
+include('connexionBdd.php'); 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<title>Accepter billet</title>
+</head>
+<body>
+
+<?php
+$reqBillet = $bdd->prepare('SELECT id, titre, contenu, DATE_FORMAT(date_creation, "le %d/%m%Y à %Hh%imin%ss") AS date FROM billets WHERE id = ?');
+$reqBillet->execute(array($_GET['id']));
+
+$donnees = $reqBillet->fetch();
+
+if(!isset($donnees['contenu']))
+{
+	echo 'Une erreur s\'est produite';
+}
+?>
+
+<h3><?php echo htmlspecialchars($donnees['titre']); ?> <em> - <?php echo htmlspecialchars($donnees['date']); ?></em></h3>
+	<span class="news">
+		<p>
+			<?php echo nl2br(htmlspecialchars($donnees['contenu'])); ?>
+		</p>
+	</span>
+		<a href="valider.php?id=<?php echo $donnees['id']; ?>">Valider</a> - <a href="supprimer.php?id=<?php echo$donnees['id']; ?>">Supprimer</a>
+
+</body>
+</html>
