2c40f6081b0c86e1c01a8a8a088ba9910595547b
Benoît Puech <benoit.puech@yahoo.fr>
2020-11-28 18:01:34 +0100

    Modification pour répondre à l'architecture MVC

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
+require('modele.php');
 
-include('connexionBdd.php');
-
-$reqBillet = $bdd->query("SELECT id, titre, contenu, DATE_FORMAT(date_creation, 'le %d/%m/%Y à %Hh%imin%ss') AS date FROM billets WHERE accepte = 1 ORDER BY date_creation DESC LIMIT 0,5");
+$reqBillet = getBillets();
 
 require('affichageAccueil.php');
 ?>
